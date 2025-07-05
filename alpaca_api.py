85701c2092f223356b12030b2c91fb18f93e12f4
Ryan Mitchell <ryansmitchell2001@gmail.com>
2025-07-05 11:49:18 -0400

    Delete alpaca_api.py

--- alpaca_api.py
+++
@@ -1,9 +0,0 @@
-# alpaca_api.py
-
-import alpaca_trade_api as tradeapi
-
-API_KEY = 'PKB1UY8JYZJS5EMLR2JS'
-API_SECRET = '07dMk3HRAQjtv2ntWiKE50J1hzKTAos0Ce7FaLFQ'
-BASE_URL = 'https://paper-api.alpaca.markets'
-
-api = tradeapi.REST(API_KEY, API_SECRET, base_url=BASE_URL, api_version='v2')
